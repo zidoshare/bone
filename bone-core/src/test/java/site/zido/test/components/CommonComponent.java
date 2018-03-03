2ff6692e03689c3caa8935101eddaf7c58153172
zido <wuhongxu1208@gmail.com>
2018-03-03 23:34:57 +0800

    Better test cases

---
+++ bone-core/src/test/java/site/zido/test/components/CommonComponent.java
@@ -0,0 +1,49 @@
+package site.zido.test.components;
+
+import site.zido.bone.core.beans.annotation.Component;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@Component(id = "common")
+public class CommonComponent {
+    private One one;
+    private Two two;
+    private Three three;
+    @Inject
+    private One otherOne;
+
+    public CommonComponent(One one) {
+        this.one = one;
+    }
+
+    @Inject
+    public void setTwo(Two two) {
+        this.two = two;
+    }
+
+    @Inject
+    public void setThree(@Named("three") Three three) {
+        this.three = three;
+    }
+
+    public One getOne() {
+        return one;
+    }
+
+    public Two getTwo() {
+        return two;
+    }
+
+    public Three getThree() {
+        return three;
+    }
+
+    public One getOtherOne() {
+        return otherOne;
+    }
+
+    public void setOtherOne(One otherOne) {
+        this.otherOne = otherOne;
+    }
+}
